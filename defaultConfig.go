915a5161d2c69ba238a8ea3fcc22f10f307db4aa
zhiheng.jiang <zhiheng.jiang@ttpai.cn>
2020-01-17 17:18:05 +0800

    保密

--- defaultConfig.go
+++ defaultConfig.go
@@ -2,10 +2,10 @@ package main
 
 //默认配置
 const (
-	_un = "zhiheng.jiang"
-	_up = "Jaiz1234"
-	_c  = "xiqiang.zhao@ttpai.cn;miao.jiang@ttpai.cn"
-	_d  = "平台开发部"
-	_n  = "姜志恒"
-	_r  = "lingyun.shan@ttpai.cn"
+	_un = "xxx"
+	_up = "xxx"
+	_c  = "xx@xx.cn;xx@xx.cn"
+	_d  = "xx部"
+	_n  = "xxx"
+	_r  = "xx@xx.cn"
 )
